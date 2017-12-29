c62aee93fc6c6d5c43e32ec52771ef12f2ef90b2
yiyinsong <390396674@qq.com>
2017-12-29 18:57:31 +0800

    2017-12-29

---
+++ standard/app/src/test.js
@@ -0,0 +1,13 @@
+var test = function (student) {
+    console.log(student);
+};
+test(5559);
+var Human = (function () {
+    function Human() {
+    }
+    return Human;
+}());
+var arr = [1, 2, 3];
+var arr2 = [1, 2, 3, 4, 5, 6];
+var arr3 = ['1', 2, 1, 1, 1];
+//# sourceMappingURL=test.js.map
--- standard/app/src/test.ts
+++ standard/app/src/test.ts
@@ -10,6 +10,15 @@ const test = (student: number) => {
 test(5559);
 class Human {
     constructor() {
-        
+
     }
 }
+interface Humans {
+    username: string,
+    age: number
+}
+
+let arr: number[] = [1, 2, 3];
+let arr2: Array<number> = [1, 2, 3, 4, 5, 6];
+
+let arr3: [string, number] = ['1', 2, 1, 1, 1];
